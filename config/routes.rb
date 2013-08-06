50987eb9336d6efb8799de8dc2aaa82c5335a13b
Emma <lemons154@gmail.com>
2013-08-06 15:36:08 +0100

    not much, really. Deletes ugly CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 YRS2013::Application.routes.draw do
   get "welcome/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
