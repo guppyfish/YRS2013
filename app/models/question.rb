204faef9df6abb839cc1af55d52590b3aff6fe6a
Soham Karwa <sohamkarwa@gmail.com>
2013-08-07 00:37:37 +0100

    Update Gemfile.lock for heroku

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
+	validates_presence_of :title
 	has_many :results 
 end
 
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,5 @@
 class Result < ActiveRecord::Base
+	validates_presence_of :title,  :sixteen_24_result, :question 
 	belongs_to :question   
 end
 
