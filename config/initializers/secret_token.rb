1873038db5c08e62ca53c27c934aa544696ee22a
Soham Karwa <sohamkarwa@gmail.com>
2013-08-06 10:41:55 +0100

    Delete everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YRS2013::Application.config.secret_key_base = '3c343c2393e96297f4c91559f7a7c26bb8f1a5e8b96a38b0b3ad17d8d5f305637e2070f8eb1266e9996e5deeaf8207ac551bc93f5a379cf732d6e0eefa3dd26b'
